7d52429810c7cffbf4f95d0fe22fe0f13e76d656
damajojung <jungdamajo@gmail.com>
2022-10-11 14:05:14 +0200

    saving

--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -1,7 +1,8 @@
-from solcx import compile_standard, install_solc
+from solcx import compile_standard
+import json
+from web3 import Web3
 
 # solcx.install_solc("0.8.0")
-import json
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -22,4 +23,28 @@ compiled_sol = compile_standard(
     solc_version="0.8.0",
 )
 
-print(compiled_sol)
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# Get bytecode (Walk down the json file -> ladder structure)
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get ABI
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+#####
+# DEPLOY
+#####
+
+# Where should we deploy it to? We can do it with Ganache
+# for connecting with ganache
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 5777
+my_address = "0x69ba60500a4335ef48524A0605ec0DB6438e4440"
+private_key = "0xf37f163310b25931f88fe6793298707170e2bb07a35eec8723d438faf5ca83fe"
+
+# Create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+print(SimpleStorage)
