98fcd3c1503f55e5cca9961661b4b060791924e2
damajojung <jungdamajo@gmail.com>
2022-10-12 17:04:28 +0200

    saving

--- demos/brownie_simple_storage/scripts/deploy.py
+++ demos/brownie_simple_storage/scripts/deploy.py
@@ -1,8 +1,16 @@
-from brownie import accounts
+from brownie import accounts, config
 
 
 def deploy_simple_storage():
-    account = accounts[0]
+    # account = accounts[0]  If we want to use our local network with ganache
+
+    # with brownie
+    # account = accounts.load("freecodecamp-account")
+    # print(account)
+
+    # With brownie and environment variables from the env list
+    account = accounts.add(config["wallets"]["from_key"])
+    print(account)
 
 
 def main():
